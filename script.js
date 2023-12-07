203acbc383787d25053facca196cc1a02327bd0a
Avxur <153037612+Avxur@users.noreply.github.com>
2023-12-07 07:20:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,18 @@
-/* -- Glow effect -- */
+const textToRepeat = "« AVXUR » ";
+const card = document.querySelector(".card"),
+      letters = card.querySelector(".card-letters");
 
-// const blob = document.getElementById("blob");
+const handleOnMove = e => {
+  const rect = card.getBoundingClientRect(),
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
 
-// window.onpointermove = event => { 
-//   const { clientX, clientY } = event;
-  
-//   blob.animate({
-//     left: `${clientX}px`,
-//     top: `${clientY}px`
-//   }, { duration: 3000, fill: "forwards" });
-// }
+  letters.style.setProperty("--x", `${x}px`);
+  letters.style.setProperty("--y", `${y}px`);
 
-/* -- Text effect -- */
-
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-let interval = null;
-
-const screen = document.querySelector(".screen"),
-      name = document.querySelector(".name");
-
-screen.onmouseenter = event => {  
-  let iteration = 0;
-  
-  clearInterval(interval);
-  
-  interval = setInterval(() => {
-    name.innerText = name.innerText
-      .split("")
-      .map((letter, index) => {
-        if(index < iteration) {
-          return name.dataset.value[index];
-        }
-      
-        return letters[Math.floor(Math.random() * 26)]
-      })
-      .join("");
-    
-    if(iteration >= name.dataset.value.length){ 
-      clearInterval(interval);
-    }
-    
-    iteration += 1 / 3;
-  }, 30);
+  // Repeat "« AVXUR »" infinitely
+  letters.innerText = textToRepeat.repeat(1500);    
 }
+
+card.addEventListener("mousemove", e => handleOnMove(e));
+card.addEventListener("touchmove", e => handleOnMove(e.touches[0]));
